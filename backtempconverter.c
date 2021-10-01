2555a7bd77f83c028d35603ebc3d7fd0ad12f224
AlexPegg <alexpeggsailing@gmail.com>
2021-10-01 10:30:06 +0100

    missed one

--- backtempconverter.c
+++ backtempconverter.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
  
 int main(void) {
-    float F;
+    float F; //store the Fahernehit value
     printf("Please enter a temperature in Fahrenheit: ");
-    scanf("%f", &F);
-    float C;
-    C = (F - 32.0) * (5.0 / 9.0);
-    printf("Temperature in Celcius: %.2f", C);
+    scanf("%f", &F); //read value from console and store in F var
+    float C; //store celcius value
+    C = (F - 32.0) * (5.0 / 9.0); //convert F to C
+    printf("Temperature in Celcius: %.2f", C);  //print celcius value to console
  
     return 0;
 }
